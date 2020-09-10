569efac56c0b6585f349ad342dd291763153387a
Шелков Дмитрий <bobyorrr@gmail.com>
2020-09-10 15:10:45 +0300

    адаптивка calculator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,21 +9,29 @@ burgerButton.addEventListener("click", () => {
 
 // Табы
 const tabsButton = document.querySelectorAll(".stages-work__button");
-const tabsContainer = document.querySelectorAll(".stages-work__tabs-item");
+const tabsContainerSW = document.querySelectorAll(".stages-work__tabs-item");
 
-for (const i of tabsButton) {
-	i.addEventListener("click", () => {
-		for (const b of tabsButton) {
-			b.classList.add("button_hgost-neutral");
-		}
-		i.classList.remove("button_hgost-neutral");
+const tabsButtonCalc = document.querySelectorAll(".calculator__head-booton");
+const tabsContainerCalc = document.querySelectorAll(".calculator__body");
 
-		for (const c of tabsContainer) {
-			c.classList.remove("tab-container-active");
-		}
-		document.querySelector("#" + i.value).classList.add("tab-container-active");
-	});
+function mainTab(tabsButton, tabsContainer) {
+	for (const i of tabsButton) {
+		i.addEventListener("click", () => {
+			for (const b of tabsButton) {
+				b.classList.add("button_hgost-neutral");
+			}
+			i.classList.remove("button_hgost-neutral");
+
+			for (const c of tabsContainer) {
+				c.classList.remove("tab-container-active");
+			}
+			document.querySelector("#" + i.value).classList.add("tab-container-active");
+		});
+	}
 }
+
+mainTab(tabsButton, tabsContainerSW);
+mainTab(tabsButtonCalc, tabsContainerCalc);
 
 // Маска для телефона
 document.querySelectorAll(".phone-mask").forEach((item) => {
